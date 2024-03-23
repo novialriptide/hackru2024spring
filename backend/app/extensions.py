7a4a19e69d39eb15e6f444290c5eee779e54b6ed
Kevin Monisit <monisitkevin@gmail.com>
2024-03-23 17:25:15 -0400

    Delete backend directory
    i propse this

--- backend/app/extensions.py
+++
@@ -1,6 +0,0 @@
-
-from flask_login import LoginManager
-
-# Create a new instance of the LoginManager class. This object will be used to manage user authentication.
-login_manager = LoginManager()
-
