2cb3257b0ad7baa67843df94faf5278475d87310
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-03-23 13:45:23 -0400

    React + TypeScript + Tailwind CSS + Prettier

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-center text-3xl font-bold text-red-600 underline">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
